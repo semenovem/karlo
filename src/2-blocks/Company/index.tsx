74a9668a86c396a5394447183fd2f40067384603
Semenov Evgeny <semenovem@gmail.com>
2022-10-17 19:49:30 +0300

    wip

--- src/2-blocks/Company/index.tsx
+++ src/2-blocks/Company/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import css from './style.module.css';
+
 import BlockLayer from '5-cmp/BlockLayer';
 import Poster from '5-cmp/Poster2';
 import TextPanel, { Paragraph as P } from '5-cmp/Text';
@@ -12,12 +14,15 @@ export default function BlockCompany(p: IBlockPageProps) {
     <BlockLayer className='g-bg-linear-main'>
       <Poster bgImg={topImg} header='О КОМПАНИИ' classname='i-after-poster-bottom' />
 
-      <TextPanel classname=''>
-        <P>Папа Карло осуществляет грузоперевозки с 2010 года </P>
-        <P>В компании трудится 100500 хороших людей. Мы делам хорошую доставку</P>
-        <P>Обратившись к нам, вы не пожалеете</P>
-        <P>С надеждой на сотрудничество Ваш Анатолий</P>
-      </TextPanel>
+      <div className={css.txt}>
+        <TextPanel classname=''>
+          <P>ИП Глазков осуществляет грузоперевозки с 2011 года</P>
+          <P>Свой автопарк, состояший из более 10 грузовых машин</P>
+          <P>Штат компании составляет 25 человек (водители-экспедиторы, сборщики, грузчики</P>
+          <P>Основной вид деятельности - комплексные услуги по доставке, сборке для организаций</P>
+          <P>С надеждой на сотрудничество Ваш Анатолий</P>
+        </TextPanel>
+      </div>
     </BlockLayer>
   );
 }
